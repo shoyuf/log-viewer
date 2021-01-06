dd18304a8ba006d7d0af81bdb367c719a749e495
shoyuf <shoyuf@shoyuf.top>
2021-01-06 16:07:55 +0800

    feat: use ansicolor replace ansiParser.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,28 @@
-import ansiParse from './ansi-parse'
+import ansi from 'ansicolor'
+ansi.rgb = {
+  black: [0, 0, 0],
+  darkGray: [129, 131, 131],
+  lightGray: [242, 242, 242],
+  white: [203, 204, 205],
+
+  red: [194, 54, 33],
+  lightRed: [252, 57, 31],
+
+  green: [37, 188, 36],
+  lightGreen: [49, 231, 34],
+
+  yellow: [173, 173, 39],
+  lightYellow: [234, 236, 35],
+
+  blue: [73, 46, 225],
+  lightBlue: [88, 51, 255],
+
+  magenta: [211, 56, 211],
+  lightMagenta: [180, 0, 158],
+
+  cyan: [51, 187, 200],
+  lightCyan: [97, 214, 214]
+}
 
 const ENCODED_NEWLINE = /\r\n|\r|\n(?!\u0008)/
 
@@ -6,16 +30,26 @@ const ENCODED_NEWLINE = /\r\n|\r|\n(?!\u0008)/
 // http://jafrog.com/2013/11/23/colors-in-terminal.html
 // https://en.wikipedia.org/wiki/ANSI_escape_code
 
-export const split2Lines = str => str.split(ENCODED_NEWLINE)
+const split2Lines = str => str.split(ENCODED_NEWLINE)
 
+export {ansi, split2Lines}
+
+/**
+ * @typedef {Object} ParsedSpan
+ * @property {string} text
+ * @property {string} css
+ * @property {boolean} [italic]
+ * @property {boolean} [bold]
+ * @property {string} [color]
+ * @property {string} [bgColor]
+ */
+
+/**
+ * @param {string} log
+ * @returns {ParsedSpan[]}
+ */
 export default log => {
-  const stringLines = split2Lines(log)
-  const stringLinesText = []
-  stringLines.forEach(line => {
-    if (!line) {
-      return
-    }
-    stringLinesText.push(ansiParse(line))
-  })
-  return stringLinesText
+  if (!log) return []
+  const stringLines = split2Lines(log).filter(e => e)
+  return stringLines.map(line => ansi.parse(line).spans)
 }
--- test/utils/ansi-parse.test.js
+++ test/utils/ansi-parse.test.js
@@ -1,55 +1,43 @@
-import ansiParse from '@/utils/ansi-parse.js'
-
-describe('src/utils/ansi-parse.js', () => {
+import {ansi} from '@/utils'
+describe('ansicolor', () => {
   it('解释日志字符串为可读数据', () => {
     const log =
       'normal\x1b[30mcolor is black\x1b[0m\x1b[31;1mcolor is red and bold is true\x1b[0m\x1b[32;3mcolor is green and italic is true\x1b[0m\x1b[33;4mcolor is yellow and underline is true\x1b[0m\x1b[30;47mcolor is black and background is white\x1b[0m\x1b[30;39mcolor is reset\x1b[0m\x1b[40;49mbackground is reset\x1b[0m\x1b[1;22mbold is false\x1b[0m\x1b[3;23mitalic is false\x1b[0m\x1b[4;24munderline is false\x1b[0m'
-    const result = ansiParse(log)
-
-    expect(result[0]).toStrictEqual({
-      text: 'normal'
-    })
-    expect(result[1]).toStrictEqual({
-      foreground: 'black',
+    const result = ansi.parse(log).spans
+    expect(result[0]).toMatchObject({css: '', text: 'normal'})
+    expect(result[1]).toMatchObject({
+      css: 'color:rgba(0,0,0,1);',
+      color: {name: 'black'},
       text: 'color is black'
     })
-    expect(result[2]).toStrictEqual({
-      foreground: 'red',
+    expect(result[2]).toMatchObject({
+      css: 'font-weight: bold;color:rgba(252,57,31,1);',
       bold: true,
+      color: {name: 'red', bright: true},
       text: 'color is red and bold is true'
     })
-    expect(result[3]).toStrictEqual({
-      foreground: 'green',
-      italic: true,
-      text: 'color is green and italic is true'
+    expect(result[3]).toMatchObject({
+      css: 'font-style: italic;color:rgba(37,188,36,1);',
+      color: {name: 'green'},
+      text: 'color is green and italic is true',
+      italic: true
     })
-    expect(result[4]).toStrictEqual({
-      foreground: 'yellow',
-      underline: true,
-      text: 'color is yellow and underline is true'
+    expect(result[4]).toMatchObject({
+      css: 'text-decoration: underline;color:rgba(173,173,39,1);',
+      color: {name: 'yellow'},
+      text: 'color is yellow and underline is true',
+      underline: true
     })
-    expect(result[5]).toStrictEqual({
-      foreground: 'black',
-      background: 'white',
+    expect(result[5]).toMatchObject({
+      css: 'color:rgba(0,0,0,1);background:rgba(242,242,242,1);',
+      color: {name: 'black'},
+      bgColor: {name: 'lightGray'},
       text: 'color is black and background is white'
     })
-    expect(result[6]).toStrictEqual({
-      text: 'color is reset'
-    })
-    expect(result[7]).toStrictEqual({
-      text: 'background is reset'
-    })
-    expect(result[8]).toStrictEqual({
-      bold: false,
-      text: 'bold is false'
-    })
-    expect(result[9]).toStrictEqual({
-      italic: false,
-      text: 'italic is false'
-    })
-    expect(result[10]).toStrictEqual({
-      underline: false,
-      text: 'underline is false'
-    })
+    expect(result[6]).toMatchObject({css: '', text: 'color is reset'})
+    expect(result[7]).toMatchObject({css: '', text: 'background is reset'})
+    expect(result[8]).toMatchObject({css: '', text: 'bold is false'})
+    expect(result[9]).toMatchObject({css: '', text: 'italic is false'})
+    expect(result[10]).toMatchObject({css: '', text: 'underline is false'})
   })
 })
